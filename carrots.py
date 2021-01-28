38dd8a015474b8fd0c83d6c14fa0d324ea362b35
Revathy-2001 <77260440+Revathy-2001@users.noreply.github.com>
2021-01-28 15:00:00 +0530

    Add files via upload

---
+++ carrots.py
@@ -0,0 +1,8 @@
+
+lst = list(input().split())
+i1 = int(lst[0])
+i2 = int(lst[1])
+for i in range(i1):
+  st = input()
+print(i2)
+
---
+++ carrots.rb
@@ -0,0 +1,6 @@
+
+x,y= gets.chomp.split.map(&:to_i)
+for i in (1..x)
+  st = gets.to_s
+end
+print(y)
